4ef8acc4a37ff64d60e16b2b80f6d6088a39bfb7
Gangsta <sashika25@mail.ru>
2017-03-06 15:30:21 +0300

    lijjjoim

---
+++ src/MyClass/Tests.java
@@ -0,0 +1,25 @@
+package MyClass;
+
+import org.junit.Test;
+
+import static MyClass.XXOO.Move.O;
+import static MyClass.XXOO.Move.X;
+import static org.junit.Assert.*;
+
+public class Tests {
+    @Test
+    public void theLongestLine() {
+        XXOO field = new XXOO(3,3);
+        field.makeTurn(0,0);
+        field.makeTurn(1,1);
+        field.makeTurn(1,0);
+        field.makeTurn(2,0);
+        field.makeTurn(2,1);
+        field.makeTurn(2,2);
+        field.makeTurn(1,2);
+        field.makeTurn(0,2);
+        field.makeTurn(0,1);
+        assertEquals(X,field.get(0,0));
+
+    }
+}
--- src/MyClass/XXOO.java
+++ src/MyClass/XXOO.java
@@ -8,7 +8,7 @@ public class XXOO {
         X, O;
 
 
-        public Move  opposite() {
+        public Move opposite() {
             if (this == X) return O;
             else return X;
         }
@@ -40,92 +40,93 @@ public class XXOO {
             return result1;
         }
     }
-    Board field = new Board(3,3);
 
+    private final int width;
+    private final int hight;
+    private final Map<Cell, Move> moves = new HashMap<>();
+    private Move turn = Move.X;
 
-    public static class Board {
-        private final int width;
-        private final int hight;
-        private final Map<Cell, Move> moves = new HashMap<>();
-        private Move turn = Move.X;
+    public XXOO(int width, int hight) {
+        this.width = width;
+        this.hight = hight;
+    }
 
-        public Board(int width, int hight) {
-            this.width = width;
-            this.hight = hight;
+    public Move get(int x, int y) {
+        return get(new Cell(x, y));
+    }
+
+    private Move get(Cell cell) {
+        return moves.get(cell);
+    }
+
+    public Move getMove() {
+        return turn;
+    }
+
+    public XXOO() {
+        this(3, 3);
+    }
+
+    public void makeTurn(int x, int y) {
+        if (x < 0 || x >= width || y < 0 || y >= hight) throw new IllegalArgumentException();
+        for (x = 0; x < width; x++) {
+            for (y = 0; y < hight; y++) {
+                Cell cell = new Cell(x, y);
+                moves.put(cell, turn);
+                turn = turn.opposite();
+            }
         }
+    }
 
-        public Move get(int x, int y) {
-            return get(new Cell(x, y));
-        }
 
-        private Move get(Cell cell) {
-            return moves.get(cell);
-        }
-
-        public Move getMove() {
-            return turn;
-        }
-
-        public void makeTurn(int x, int y) {
-            if (x < 0 || x >= width || y < 0 || y >= hight) throw new IllegalArgumentException();
-            for (x = 0; x < width; x++) {
-                for (y = 0; y < hight; y++) {
-                    Cell cell = new Cell(x, y);
-                        moves.put(cell, turn);
-                        turn = turn.opposite();
+    public void clearCell(int x, int y) {
+        if (x < 0 || x >= width || y < 0 || y >= hight) throw new IllegalArgumentException();
+        for (x = 0; x < width; x++) {
+            for (y = 0; y < hight; y++) {
+                Cell cell = new Cell(x, y);
+                if (moves.containsKey(cell)) {
+                    moves.remove(cell, turn);
                 }
             }
         }
+    }
 
 
-        public void clearCell(int x, int y) {
-            if (x < 0 || x >= width || y < 0 || y >= hight) throw new IllegalArgumentException();
-            for (x = 0; x < width; x++) {
-                for (y = 0; y < hight; y++) {
-                    Cell cell = new Cell(x, y);
-                    if (moves.containsKey(cell)) {
-                        moves.remove(cell, turn);
-                    }
-                }
-            }
-        }
-
-
-        public int theLongestLine(Move move) {
-            int result = 0;
-            int x = 0;
-            while (x < width) {
-                int count = 0;
-                for (int y = 0; y < hight - 1; y++) {
-                    Cell cell = new Cell(x,y);
-                    Cell next = new Cell(x,y+1);
-                    if (move.equals(get(cell)) && move.equals(get(next)))
-                        count++;
-
-                }
-                Cell last = new Cell(x,hight-1);
-                if(move.equals(get(last)))
+    public int theLongestLine(Move move) {
+        int result = 0;
+        int x = 0;
+        while (x < width) {
+            int count = 0;
+            for (int y = 0; y < hight - 1; y++) {
+                Cell cell = new Cell(x, y);
+                Cell next = new Cell(x, y + 1);
+                if (move.equals(get(cell)) && move.equals(get(next)))
                     count++;
-                if (count > result) result = count;
-                x++;
-            }
-            int y = 0;
-            while (y < hight) {
-                int count = 0;
-                for (x = 0; x < width - 1; x++) {
-                    Cell cell = new Cell(x,y);
-                    Cell next = new Cell(x+1,y);
-                    if (move.equals(get(cell)) && move.equals(get(next)))
-                        count++;
 
-                }
-                Cell last = new Cell(width-1,y);
-                if(move.equals(get(last)))
-                    count++;
-                if (count > result) result = count;
-                y++;
             }
-            return result;
+            Cell last = new Cell(x, hight - 1);
+            if (move.equals(get(last)))
+                count++;
+            if (count > result) result = count;
+            x++;
         }
+        int y = 0;
+        while (y < hight) {
+            int count = 0;
+            for (x = 0; x < width - 1; x++) {
+                Cell cell = new Cell(x, y);
+                Cell next = new Cell(x + 1, y);
+                if (move.equals(get(cell)) && move.equals(get(next)))
+                    count++;
+
+            }
+            Cell last = new Cell(width - 1, y);
+            if (move.equals(get(last)))
+                count++;
+            if (count > result) result = count;
+            y++;
+
+        }
+        return result;
     }
 }
